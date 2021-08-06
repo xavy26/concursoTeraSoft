fd31368731d5d7eb28f11234f248dfeb5c7d93c7
Jhon Carrión <jhon.carrion@unl.edu.ec>
2021-08-06 14:09:14 -0500

    rutas

---
+++ routes/functionary.js
@@ -0,0 +1,17 @@
+var express = require('express');
+
+const controller = require('../controllers/functionary.controller');
+
+var router = express.Router();
+
+/* GET home page. */
+router.get('/test', controller.test);
+router.get('/new', controller.register);
+// router.get('/edit/:id', controller.register);
+// router.get('/down/:id', controller.down);
+router.get('/get/:type/:search', controller.get);
+
+router.post('/update/:id', controller.update);
+router.post('/save', controller.register);
+
+module.exports = router;
